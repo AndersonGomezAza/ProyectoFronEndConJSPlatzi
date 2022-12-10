2e596ff83d284e3a3414e51cc8fedceddbe0d2c4
AndersonGomezAza <argomeza@itc.edu.co>
2022-12-10 13:48:11 -0500

    Proyecto Terminado de js al proyecto de front end developer

--- js/script.js
+++ js/script.js
@@ -4,76 +4,92 @@ const btnMenuMobile = document.querySelector('#btnMenuMobile');
 const mobileMenu = document.querySelector('.mobile-menu');
 const navbarCartIcon = document.querySelector('.navbar-shopping-card');
 const cartDetail = document.querySelector('.cart-detail');
+const productDetail = document.querySelector('.product-detail');
+const closeAsideProduct = document.querySelector(".product-detail-close");
+const asideProduct = document.querySelector(".product-detail");
 
 navEmail.addEventListener('click', toggleDesktopMenu)
 btnMenuMobile.addEventListener('click', toggleMobileMenu)
 navbarCartIcon.addEventListener('click', toggleCartDetail)
+closeAsideProduct.addEventListener('click', closeProductDetailAside)
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
-    cartDetail.classList.add('inactive');
-}
-
-function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
-    cartDetail.classList.add('inactive');
-}
-
-function toggleCartDetail() {
-    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const isCarDetailClosed = cartDetail.classList.contains('inactive');
-    cartDetail.classList.toggle('inactive');
-    mobileMenu.classList.add('inactive');
-    desktopMenu.classList.add('inactive');
-}
 
 const productList = [];
 
 productList.push({
     name: 'Bike',
     price: 120,
-    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Esto es un texto Descriptivo del producto Bike'    
 });
 productList.push({
     name: 'Pantalla',
     price: 220,
-    image: 'https://www.lg.com/cac_en/images/monitors/md07543961/D-1.jpg'
+    image: 'https://www.lg.com/cac_en/images/monitors/md07543961/D-1.jpg',
+    description: 'Esto es un texto Descriptivo del producto Pantalla'    
 });
 productList.push({
     name: 'Computador',
     price: 620,
-    image: 'https://exitocol.vtexassets.com/arquivos/ids/5347301-800-auto?v=637438626358370000&width=800&height=auto&aspect=true'
+    image: 'https://exitocol.vtexassets.com/arquivos/ids/5347301-800-auto?v=637438626358370000&width=800&height=auto&aspect=true',
+    description: 'Esto es un texto Descriptivo del producto computador'    
 });
 productList.push({
     name: 'Bike',
     price: 120,
-    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Esto es un texto Descriptivo del producto Bike'    
 });
 productList.push({
     name: 'Pantalla',
     price: 220,
-    image: 'https://www.lg.com/cac_en/images/monitors/md07543961/D-1.jpg'
+    image: 'https://www.lg.com/cac_en/images/monitors/md07543961/D-1.jpg',
+    description: 'Esto es un texto Descriptivo del producto Pantalla'    
 });
 productList.push({
     name: 'Computador',
     price: 620,
-    image: 'https://exitocol.vtexassets.com/arquivos/ids/5347301-800-auto?v=637438626358370000&width=800&height=auto&aspect=true'
+    image: 'https://exitocol.vtexassets.com/arquivos/ids/5347301-800-auto?v=637438626358370000&width=800&height=auto&aspect=true',
+    description: 'Esto es un texto Descriptivo del producto computador'    
 });
 productList.push({
     name: 'Bike',
     price: 120,
-    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Esto es un texto Descriptivo del producto Bike'    
 });
 productList.push({
     name: 'Pantalla',
     price: 220,
-    image: 'https://www.lg.com/cac_en/images/monitors/md07543961/D-1.jpg'
+    image: 'https://www.lg.com/cac_en/images/monitors/md07543961/D-1.jpg',
+    description: 'Esto es un texto Descriptivo del producto Pantalla'    
 });
 productList.push({
     name: 'Computador',
     price: 620,
-    image: 'https://exitocol.vtexassets.com/arquivos/ids/5347301-800-auto?v=637438626358370000&width=800&height=auto&aspect=true'
+    image: 'https://exitocol.vtexassets.com/arquivos/ids/5347301-800-auto?v=637438626358370000&width=800&height=auto&aspect=true',
+    description: 'Esto es un texto Descriptivo del producto computador'    
 });
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+    cartDetail.classList.add('inactive');
+    asideProduct.classList.add('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+    cartDetail.classList.add('inactive');
+    asideProduct.classList.add('inactive');
+}
+
+function toggleCartDetail() {
+    cartDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    asideProduct.classList.add('inactive');
+}
+
 
 function showProducts(arrayProduct) {
     for (const product of arrayProduct) {
@@ -83,6 +99,8 @@ function showProducts(arrayProduct) {
             const imgProduct = document.createElement('img');
             imgProduct.setAttribute('src', product.image);
             imgProduct.classList.add('product-img')
+            imgProduct.addEventListener('click', openProductDetailAside)
+
             const productInfo = document.createElement('div');
             productInfo.classList.add('content-product');
     
@@ -109,8 +127,18 @@ function showProducts(arrayProduct) {
     
         const cardsContainer = document.querySelector('.cards-container');
         cardsContainer.appendChild(productCard);
-    
     }
+}
+
+function openProductDetailAside() {
+    asideProduct.classList.remove('inactive');
+    cartDetail.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+}
+
+function closeProductDetailAside() {
+    asideProduct.classList.add('inactive');
 }
 
 showProducts(productList);
